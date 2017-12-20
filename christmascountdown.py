24c593d52ae9a9fa74a847565f96f32e1f39eb53
Brian Burton <brian-burton@users.noreply.github.com>
2017-12-20 23:47:18 +0000

    Moved clear_buffer() to prevent garbage output

--- christmascountdown.py
+++ christmascountdown.py
@@ -10,12 +10,12 @@ try:
   scrollphat.set_brightness(10)
   last_time=0
   while True:
-    scrollphat.clear_buffer()
     my_delta = datetime.datetime(year=YEAR,month=12,day=25,hour=0,minute=0) - datetime.datetime.now()
     my_delta_days = my_delta.days
     my_delta_hours = math.floor(my_delta.seconds/3600)
     hour_string = '{0}'.format(my_delta_days * 24 + my_delta_hours)
     if hour_string != last_time:
+      scrollphat.clear_buffer()
       for i in range(11):
         scrollphat.scroll()
         time.sleep(0.01)
